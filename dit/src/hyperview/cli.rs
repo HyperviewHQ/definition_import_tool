ab46c48af63ff15dc2babdc85f2e5c8c3115858c
Rami Jebara <rami.jebara@hyperviewhq.com>
2023-05-13 20:25:37 -0700

    added config path loading test

--- dit/src/hyperview/cli.rs
+++ dit/src/hyperview/cli.rs
@@ -225,6 +225,7 @@ mod tests {
     use std::fs::File;
     use std::io::BufReader;
     use std::io::Read;
+    use std::io::Write;
     use std::path::MAIN_SEPARATOR_STR;
     use tempfile::NamedTempFile;
 
@@ -250,6 +251,27 @@ mod tests {
         );
 
         assert_eq!(config_path, expected_path);
+    }
+
+    #[test]
+    fn test_app_config_loading() {
+        let mut tmp_file = NamedTempFile::new().unwrap();
+        write!(tmp_file, r#"client_id = "test_id"
+client_secret = "test_secret"
+scope = "test_scope"
+auth_url = "https://test_auth_url"
+token_url = "https://test_token_url"
+instance_url = "https://test_instance_url"
+"#).unwrap();
+
+        let config: AppConfig = confy::load_path(tmp_file.path().to_str().unwrap().to_string()).unwrap();
+
+        assert_eq!(config.client_id, "test_id");
+        assert_eq!(config.client_secret, "test_secret");
+        assert_eq!(config.scope, "test_scope");
+        assert_eq!(config.auth_url, "https://test_auth_url");
+        assert_eq!(config.token_url, "https://test_token_url");
+        assert_eq!(config.instance_url, "https://test_instance_url");
     }
 
     #[test]
